907282e4ada43e9030a6e1e13429a735d22fb693
Kriskoin <cris7775@gmail.com>
2024-11-09 18:27:38 -0600

    wip

---
+++ test7.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "bash version: ${BASH_VERSION}..."
+x=1 
+while [ $x -le 10 ]  ; 
+do
+ echo "hola este es el valor de x: $x "
+ ((x++))
+done
+
+for i in 1 2 3 4 5
+do
+    echo " Valor de i: $i"
+done
+
+for j in {1..5}
+do
+    echo "Welcome $j times"
+done
+
+for y in {0..10..2}
+do
+    echo "this is the y: $y value"
+done
+
+
+g = 1
+until [ $g -gt 10 ]
+do  
+    echo "This is f: $g value"
+    ((g++))
+done
+
+
+xx = 10
+
+until [ 2 -gt $xx ]
+do
+    echo "El valor de xx : $xx"
+    ((xx--))
+done
